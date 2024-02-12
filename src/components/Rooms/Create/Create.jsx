3d08d3be05d2185d1a745fb6681cbad706d2aecb
rishirathoreee <rishi.codemeg@gmail.com>
2024-02-12 10:57:07 +0530

    added new change sin ui

--- src/components/Rooms/Create/Create.jsx
+++ src/components/Rooms/Create/Create.jsx
@@ -63,7 +63,7 @@ const Create = () => {
       <ImageUploader />
 
       <div className='space-x-2'>
-      <button type="button" className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 ">Add Room</button>
+      <button type="button" className="text-white bg-red-500 hover:bg-red-700 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 ">Add Room</button>
       <button type="button" className="py-2.5 px-5 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 focus:z-10 focus:ring-4 focus:ring-gray-200 ">Cancel</button>
       </div>
       </div>
--- src/components/common/Navbar/Components/Profile.jsx
+++ src/components/common/Navbar/Components/Profile.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react'
-import profile from '../../../../assets/images/profile.avif'
+import profile from '../../../../assets/images/dp.jpg'
 import {User,SignOut,Gear,Storefront} from 'phosphor-react'
 const Profile = () => {
     const [show,setShow] = useState(false)
@@ -24,32 +24,32 @@ const Profile = () => {
   return (
     <div>
       <div className='relative'>
-        <div ref={imageRef} onClick={()=>{setShow(p=>!p)}} className='w-max p-1 cursor-pointer items-center flex min overflow-hidden rounded-md ring-1 ring-black/5 text-[12px] text-white font-semibold capitalize tracking-tighter'>
-            <img src={profile} className='object-cover w-8 h-8' alt="" />
+        <div ref={imageRef} onClick={()=>{setShow(p=>!p)}} className='w-max p-1 gap-2 cursor-pointer items-center flex min overflow-hidden rounded-md ring-1 ring-black/5 text-[12px] text-white font-semibold capitalize tracking-tighter'>
+            <img src={profile} className='object-cover w-8 h-8 rounded-md' alt="" />
             <p className='font-semibold select-none text-black text-[10px]'>Rishi Rathore</p>
         </div>
-        <div ref={boxRef} className={`w-60 top-12 right-0 bg-white ring-1 ring-black/5 shadow-xl rounded-lg shadow-black/5 absolute h-min duration-200
+        <div ref={boxRef} className={`w-52 top-12 right-0 bg-white ring-1 ring-black/5 shadow-xl rounded-lg shadow-black/5 absolute h-min duration-200
         ${show ? 'visible opacity-100 scale100 translate-y-0' : '-translate-y-4 scal-75 invisible opacity-0'}`}>
         <ul className='p-1'>
           
-          <li className='cursor-pointer hover:bg-gray-100/60 duration-500 text-[12px] flex items-center gap-2 font-light p-2'>
+          <li className='cursor-pointer hover:bg-gray-100/60 duration-200 text-[12px] flex items-center gap-2 font-light p-2'>
             <User size={13}/>
-            <p className='font-semibold text-[12px]'>My Profile</p>
+            <p className='font-medium text-[12px]'>My Profile</p>
           </li>
 
-          <li className='cursor-pointer hover:bg-gray-100/60 duration-500 text-[12px] flex items-center gap-2 font-light p-2'>
+          <li className='cursor-pointer hover:bg-gray-100/60 duration-200 text-[12px] flex items-center gap-2 font-light p-2'>
             <Gear size={13}/>
-            <p className='font-semibold text-[12px]'>Account Settings</p>
+            <p className='font-medium text-[12px]'>Account Settings</p>
           </li>
 
-          <li className='cursor-pointer hover:bg-gray-100/60 duration-500 text-[12px] flex items-center gap-2 font-light p-2'>
+          <li className='cursor-pointer mb-2 border-b-[1px] border-gray-50 hover:bg-gray-100/60 duration-200 text-[12px] flex items-center gap-2 font-light p-2'>
             <Storefront size={13}/>
-            <p className='font-semibold text-[12px]'>Store Management</p>
+            <p className='font-medium text-[12px]'>Store Management</p>
           </li>
 
-          <li className='cursor-pointer hover:bg-gray-100/60 duration-500 text-[12px] rounded-md flex items-center justify-center ring-1 ring-black/5 gap-2 font-light p-2'>
+          <li className='cursor-pointer hover:bg-gray-100/60 duration-200 text-[12px] rounded-md flex items-center justify-center ring-1 ring-black/5 gap-2 font-light p-2'>
             <SignOut size={13}/>
-            <p className='font-semibold text-[12px]'>Sign out of all accounts</p>
+            <p className='font-medium text-[12px]'>Sign out of all accounts</p>
           </li>
           
         </ul>
